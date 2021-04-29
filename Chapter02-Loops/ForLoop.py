cb7b5b87ae9f44cf8bf1770f5622e5f313a0ab2d
Werison Aparecido Fernandes <waparecf@everis.com>
2021-04-29 18:11:28 -0300

    Estruturas de repeticao

---
+++ Chapter02-Loops/ForLoop.py
@@ -0,0 +1,6 @@
+for numero in range(1, int(input("Digite um numero para determinar o fim: ")), 1):
+    print("\t" + str(numero))
+
+print("FIM**********************************************************************")
+for num in range(1, 10, 1):
+    print("\t" + str(num))
---
+++ Chapter02-Loops/Tabuada.py
@@ -0,0 +1,5 @@
+tabuada = int(input("Digite um número para exibir a tabuada: "))
+print("Tabuada do número ", tabuada)
+
+for valor in range(0, 11, 1):
+    print(str(tabuada) + " x " + str(valor) + " = " + str((tabuada*valor)))
