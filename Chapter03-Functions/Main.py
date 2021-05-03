cc9aa9f9b7c6d87de4957ed6980b20cca6a899f7
Werison Aparecido Fernandes <waparecf@everis.com>
2021-05-03 11:08:12 -0300

    Learning functions and rank with yield iterable

---
+++ Chapter03-Functions/Main.py
@@ -0,0 +1,18 @@
+from Chapter03_Functions.Functions import *
+# minhaLista = []
+# print("Preenchendo")
+# preencherInventario(minhaLista)
+# print("Exibindo")
+# exibirInventario(minhaLista)
+# print("Pesquisando")
+# localizarPorNome(minhaLista)
+# print("Alterando")
+# depreciarPorNome(minhaLista, 20)
+# print("Excluindo")
+# print(excluirPorSerial(minhaLista))
+# exibirInventario(minhaLista)
+# print("Resumindo")
+# resumirValores(minhaLista)
+
+for index in rank(2, 3, 2):
+    print(index)
