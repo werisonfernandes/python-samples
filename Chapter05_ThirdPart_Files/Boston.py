99fc2175321b7493304900d848ff88daedc50085
Werison Aparecido Fernandes <waparecf@everis.com>
2021-05-04 18:56:29 -0300

    Trabalhando com arquivos de terceiros

---
+++ Chapter05_ThirdPart_Files/Boston.py
@@ -0,0 +1,6 @@
+# -*- encoding: utf-8
+with open("../Temp/economic-indicators.csv", "r") as boston:
+    total = 0
+    for linha in boston.readlines()[1:-1]:
+        total = total + float(linha.split(",")[3])
+    print("O total de voos é: ", total)
