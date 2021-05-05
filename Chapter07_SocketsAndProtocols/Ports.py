1d2c47ff861134133353a68473ee5600ef8a743f
Werison Aparecido Fernandes <waparecf@everis.com>
2021-05-05 17:58:59 -0300

    Trabalhando com sockets

---
+++ Chapter07_SocketsAndProtocols/Ports.py
@@ -0,0 +1,5 @@
+# -*- encoding: utf-8
+import socket
+print(socket.getservbyname("domain"))
+print(socket.getservbyname("http"))
+print(socket.getservbyname("ftp"))
---
+++ Chapter07_SocketsAndProtocols/Socket.py
@@ -0,0 +1,8 @@
+# -*- encoding: utf-8
+import socket
+resp="S"
+while(resp=="S"):
+    url=input("Digite uma url: ")
+    ip=socket.gethostbyname(url)
+    print("O IP referente à url informada é: ", ip)
+    resp=input("Digite <s> para continuar: ").upper()
